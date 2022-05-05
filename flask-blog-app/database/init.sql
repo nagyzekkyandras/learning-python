621dc1a79475984fa8a43fb91983d6c64f7c62de
nagyzekkyandras <nagyandris0718@gmail.com>
2022-05-05 07:47:17 +0200

    blog app added

---
+++ flask-blog-app/database/init.sql
@@ -0,0 +1,7 @@
+create database IF NOT EXISTS flog_db;
+
+use flog_db;
+
+create table users(user_id int auto_increment, first_name varchar(30), last_name varchar(30), username varchar(30) unique, email varchar(40) unique, password varchar(200), primary key(user_id));
+
+create table blog(blog_id int auto_increment, title varchar(100), author varchar(40), body varchar(1000), primary key(blog_id));
